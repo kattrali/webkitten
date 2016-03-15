172e442c919bd2cbcf8f4d2c545083017c9c7c74
Delisa Mason <iskanamagus@gmail.com>
2016-03-15 02:24:57 -0700

    Use an implicit application instance
    Move callback handling into library, clean up main

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 extern crate libc;
 extern crate gtk_sys;
+extern crate gobject_sys;
+extern crate glib_sys;
 
-use libc::c_char;
+use libc::{c_char,c_int};
 use std::ffi::CString;
 use std::mem;
-use gtk_sys::{GtkApplication,GtkWidget,GtkWindow};
-
-enum WebKitWebView {}
+use gtk_sys::{GtkWidget,GtkWindow};
 
 pub const WEBKITTEN_APP_ID: &'static str = "me.delisa.webkitten";
 pub const WEBKITTEN_TITLE: &'static str = "webkitten";
+
+type GtkWidgetCallback = unsafe extern "C" fn(*mut gtk_sys::GtkWidget,
+                                              *mut gtk_sys::GtkWidget);
+
+enum WebKitWebView {}
 
 #[link(name="webkit2gtk-4.0")]
 extern "C" {
@@ -18,7 +23,6 @@ extern "C" {
 }
 
 pub struct Application {
-    app: *mut GtkApplication,
     windows: Vec<Window>,
 }
 
@@ -29,18 +33,16 @@ pub struct Window {
 
 impl Application {
 
-    pub fn new(app: *mut GtkApplication) -> Self {
-        Application { app: app, windows: vec![] }
+    pub fn new() -> Self {
+        Application { windows: vec![] }
     }
 
-    pub fn show(&mut self) {
-        if self.windows.is_empty() {
-            self.add_window()
-        }
+    pub fn run(&mut self) {
+        with_gtk_app(|| if self.windows.is_empty() { self.add_window(); })
     }
 
     pub fn add_window(&mut self) {
-        let mut window = Window::new(self.app, WEBKITTEN_TITLE);
+        let mut window = Window::new(WEBKITTEN_TITLE);
         window.add_webview("http://delisa.me");
         window.show();
         self.windows.push(window);
@@ -49,9 +51,10 @@ impl Application {
 
 impl Window {
 
-    pub fn new(app: *mut GtkApplication, title: &'static str) -> Self {
+    pub fn new(title: &'static str) -> Self {
         let widget = unsafe {
-            let widget = gtk_sys::gtk_application_window_new(app);
+            let widget = gtk_sys::gtk_window_new(gtk_sys::GTK_WINDOW_TOPLEVEL);
+            add_widget_callback(widget, "destroy", destroy_window_callback);
             let window: *mut GtkWindow = mem::transmute(widget);
             gtk_sys::gtk_window_set_title(window,  CString::new(title).unwrap().as_ptr());
             gtk_sys::gtk_window_set_default_size(window, 800, 600);
@@ -81,3 +84,27 @@ impl Window {
     }
 }
 
+fn with_gtk_app<F>(callback: F) where F: FnOnce() -> () {
+    unsafe { gtk_sys::gtk_init(0 as *mut c_int, 0 as *mut *mut *mut c_char); }
+    callback();
+    unsafe { gtk_sys::gtk_main(); }
+}
+
+// Destroy the application when a window is closed - temporary behavior while
+// there is only one window per application.
+unsafe extern "C" fn destroy_window_callback(_: *mut GtkWidget, _: *mut GtkWidget) {
+    gtk_sys::gtk_main_quit();
+}
+
+// Add a callback to an event on a GTK+ widget
+unsafe fn add_widget_callback(widget: *mut GtkWidget,
+                              signal: &'static str,
+                            callback: GtkWidgetCallback) {
+    let gobject_widget: *mut gobject_sys::GObject = mem::transmute(widget);
+    let signal_ptr = CString::new(signal).unwrap().as_ptr();
+    let mut data = 0;
+    let gcallback: gobject_sys::GCallback = mem::transmute(Some(callback));
+    gobject_sys::g_signal_connect_data(gobject_widget, signal_ptr, gcallback,
+                                      &mut data as *mut _ as glib_sys::gpointer,
+                                      None, gobject_sys::GConnectFlags::empty());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,6 @@
-extern crate libc;
-extern crate gtk_sys;
-extern crate gio_sys;
-extern crate gobject_sys;
-extern crate glib_sys;
 extern crate webkitten;
 
-use webkitten::Application;
-use libc::c_void;
-use std::ffi::CString;
-use std::mem;
-use gtk_sys::GtkApplication;
-
-type GtkApplicationCallback = unsafe extern "C" fn(*mut gtk_sys::GtkApplication,
-                                                   *mut c_void);
-
-// Initialize a GTK+ application
 fn main() {
-    unsafe {
-        let identifier = CString::new(webkitten::WEBKITTEN_APP_ID).unwrap();
-        let flags = gio_sys::GApplicationFlags::empty();
-        let app = gtk_sys::gtk_application_new(identifier.as_ptr(), flags);
-        add_app_callback(app, "activate", Some(activate));
-
-        let gapp: *mut gio_sys::GApplication = mem::transmute(app);
-        gio_sys::g_application_run(gapp, 0, 0 as *mut *mut i8);
-
-        let gobject_app: *mut gobject_sys::GObject = mem::transmute(app);
-        gobject_sys::g_object_unref(gobject_app);
-    }
-}
-
-// Activation callback to the completion of GTK+ application initialization
-// Creates a new webkitten application instance and shows the first window
-unsafe extern "C" fn activate(app: *mut GtkApplication, _:*mut c_void) {
-    let mut webkitten = Application::new(app);
-    webkitten.show();
-}
-
-// Add a callback to an event on a GTK+ application
-unsafe fn add_app_callback(app: *mut GtkApplication, signal: &'static str,
-                           callback: Option<GtkApplicationCallback>) {
-    let gobject_app: *mut gobject_sys::GObject = mem::transmute(app);
-    let signal_ptr = CString::new(signal).unwrap().as_ptr();
-    let mut data = 0;
-    let gcallback: gobject_sys::GCallback = mem::transmute(callback);
-    gobject_sys::g_signal_connect_data(gobject_app,
-                                      signal_ptr,
-                                      gcallback,
-                                      &mut data as *mut _ as glib_sys::gpointer,
-                                      None,
-                                      gobject_sys::GConnectFlags::empty());
+    webkitten::Application::new().run();
 }
 
