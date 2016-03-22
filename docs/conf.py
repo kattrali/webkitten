41f4668b64d803011973bc46d5ceb585fe96663e
Delisa Mason <iskanamagus@gmail.com>
2016-03-22 11:57:31 -0700

    [docs] Parse markdown

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@
 import sys
 import os
 
+from recommonmark.parser import CommonMarkParser
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -33,6 +35,10 @@ extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
