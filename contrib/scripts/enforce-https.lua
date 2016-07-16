31321367fbfbf2b1ff5c3061473c5867fdde99e9
Delisa Mason <iskanamagus@gmail.com>
2016-07-16 01:31:16 -0700

    Add command using new hook
    * enforce-http redirects http links to https
    * move event slightly sooner in request loading scheme

---
+++ contrib/scripts/enforce-https.lua
@@ -0,0 +1,13 @@
+function description()
+  return "Coerces all URLs to HTTPS"
+end
+
+function on_load_uri()
+  target = requested_uri
+  log_info("Checking URI")
+  if string.find(target, "http://") then
+    target = target:gsub("http://", "https://")
+    log_info("Redirecting to HTTPS")
+    load_uri(window_index, webview_index, target)
+  end
+end
