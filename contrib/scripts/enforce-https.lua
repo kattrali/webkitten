b1e787444fcc5f981be077b0636b88d9354dd691
Delisa Mason <iskanamagus@gmail.com>
2016-07-16 16:15:16 -0700

    Add configuration option for ignoring hosts from enforce-https

--- contrib/scripts/enforce-https.lua
+++ contrib/scripts/enforce-https.lua
@@ -1,10 +1,18 @@
 function description()
-  return "Coerces all URLs to HTTPS"
+  return "Coerces HTTP URLs to HTTPS"
 end
 
+-- Redirects from HTTP to HTTPS, except where excluded in the configuration
+-- option `enforce-https.ignored-hosts`
 function on_request_uri()
   target = requested_uri
-  log_info("Checking URI")
+  ignored_hosts = lookup_strings(config_file_path, "enforce-https.ignored-hosts")
+  host = string.gmatch(string.match(requested_uri, "://(.*)"), "[^/]+")()
+  for _, ignored_host in ipairs(ignored_hosts) do
+    if ignored_host == host then
+      return
+    end
+  end
   if string.find(target, "http://") then
     target = target:gsub("http://", "https://")
     log_info("Redirecting to HTTPS")
