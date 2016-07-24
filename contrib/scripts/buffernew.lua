7848d0ac6ea0ed98813f2882d15dd437cef4f8bd
Delisa Mason <iskanamagus@gmail.com>
2016-07-24 02:33:32 -0700

    [contrib] Push start page logic into command

--- contrib/scripts/buffernew.lua
+++ contrib/scripts/buffernew.lua
@@ -1,8 +1,14 @@
+function description()
+  return "Opens a new buffer with a URL or configured start page"
+end
+
 function run()
   local target = ""
   local windex = focused_window_index()
   if #arguments > 0 then
     target = arguments[1]
+  else
+    target = lookup_string(config_file_path, "window.start-page")
   end
   open_webview(windex, target)
   return true
