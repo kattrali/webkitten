984b59ae75f23195d1b7026c18a1bb21b33f88fa
Delisa Mason <iskanamagus@gmail.com>
2016-07-23 22:18:10 -0700

    [contrib] Add completion for buffer cycling

--- contrib/scripts/buffer.lua
+++ contrib/scripts/buffer.lua
@@ -2,10 +2,25 @@ function run()
   windex = focused_window_index()
   if #arguments > 0 then
     index = tonumber(arguments[1])
-    if index >= webview_count(windex) then
+    if index > webview_count(windex) then
       return false
     end
     focus_webview(windex, tonumber(arguments[1]))
   end
   return true
+end
+
+-- Display titles for buffers
+function complete_command()
+  local titles = {}
+  local query = table.concat(arguments," ")
+  local windex = focused_window_index()
+  local total = webview_count(windex)
+  for i = 0, total - 1 do
+    title = "" .. i .. " : " .. webview_title(windex, i):gsub(","," ")
+    if string.sub(title, 1, #query) == query then
+      titles[#titles + 1] = title
+    end
+  end
+  return table.concat(titles,",")
 end
