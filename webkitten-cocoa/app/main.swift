b92bb548c69eb46bd691f8af8d9afb9479ffcfe6
Delisa Mason <iskanamagus@gmail.com>
2016-07-24 21:46:08 -0700

    [cocoa] Pipe task logs into console

--- webkitten-cocoa/app/main.swift
+++ webkitten-cocoa/app/main.swift
@@ -1,8 +1,28 @@
 import Cocoa
 
+func log(data: NSData) {
+    if let message = NSString(data: data, encoding: NSUTF8StringEncoding) {
+        NSLog("%@", message)
+    }
+}
+
 let task = NSTask()
 let bundle = NSBundle.mainBundle()
 task.launchPath = bundle.pathForResource("webkitten-cocoa", ofType: nil)
+
+let stdOut = NSPipe()
+let stdErr = NSPipe()
+
+stdOut.fileHandleForReading.readabilityHandler = { log($0.availableData) }
+stdErr.fileHandleForReading.readabilityHandler = { log($0.availableData) }
+
+task.standardOutput = stdOut
+task.standardError = stdErr
+
+task.terminationHandler = { task in
+    task.standardOutput?.fileHandleForReading.readabilityHandler = nil
+    task.standardError?.fileHandleForReading.readabilityHandler = nil
+}
 
 task.launch()
 task.waitUntilExit()
