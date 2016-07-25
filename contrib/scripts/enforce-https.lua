f0d1ea35b8a4b7d470e273e7ff1e906cb202e7db
Delisa Mason <iskanamagus@gmail.com>
2016-07-25 11:10:19 -0700

    [contrib] Skip https enforcement on local links

--- contrib/scripts/enforce-https.lua
+++ contrib/scripts/enforce-https.lua
@@ -6,6 +6,9 @@ end
 -- option `enforce-https.ignored-hosts`
 function on_request_uri()
   target = requested_uri
+  if string.find(target, ".local") or string.find(target, "localhost") then
+    return
+  end
   ignored_hosts = lookup_strings(config_file_path, "enforce-https.ignored-hosts")
   host = string.gmatch(string.match(requested_uri, "://(.*)"), "[^/]+")()
   for _, ignored_host in ipairs(ignored_hosts) do
