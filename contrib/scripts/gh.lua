29c044570801bd2d70f3b6cee1e2f190d79df9ba
Delisa Mason <iskanamagus@gmail.com>
2016-07-25 11:12:35 -0700

    [contrib] Add GitHub subdomains to argument options

--- contrib/scripts/gh.lua
+++ contrib/scripts/gh.lua
@@ -1,24 +1,48 @@
 function description()
-  return "Opens a repo or performs a search on GitHub"
+  return "Opens a repo, performs a search on GitHub, or opens a common page"
 end
 
 function run()
-  windex = focused_window_index()
-  webview_index = focused_webview_index(windex)
+  local windex = focused_window_index()
+  local webview_index = focused_webview_index(windex)
   if #arguments > 0 then
-    owner, repo = string.match(arguments[1], "([%w-]+)/([%w-]+)")
-    if owner ~= nil then
-      target = string.format("https://github.com/%s/%s", owner, repo)
-      load_uri(windex, webview_index, target)
+    if arguments[1] == "status" then
+      load_uri(windex, webview_index, "https://status.github.com")
+    elseif arguments[1] == "help" then
+      load_uri(windex, webview_index, "https://help.github.com")
+    elseif arguments[1] == "api" then
+      load_uri(windex, webview_index, "https://developer.github.com/v3/")
+    elseif arguments[1] == "dev" then
+      load_uri(windex, webview_index, "https://developer.github.com")
     else
-      query = url_encode(table.concat(arguments, " "))
-      target = table.concat({"https://github.com/search?q=", query}, "")
-      load_uri(windex, webview_index, target)
+      local owner, repo = string.match(arguments[1], "([%w-]+)/([%w-]+)")
+      if owner ~= nil then
+        target = string.format("https://github.com/%s/%s", owner, repo)
+        load_uri(windex, webview_index, target)
+      else
+        query = url_encode(table.concat(arguments, " "))
+        target = table.concat({"https://github.com/search?q=", query}, "")
+        load_uri(windex, webview_index, target)
+      end
     end
   else
     load_uri(windex, webview_index, "https://github.com")
   end
   return true
+end
+
+function complete_command()
+  local subcommands = {"api","dev","help", "status"}
+  local query = table.concat(arguments," ")
+  if #query == 0 then
+    return table.concat(subcommands,",")
+  end
+  for i, cmd in pairs(subcommands) do
+    if string.sub(cmd, 1, #query) == query then
+      return cmd
+    end
+  end
+  return ""
 end
 
 function url_encode(str)
