fde6f47856bcae819822effabf09bdcf964fc6b6
Delisa Mason <iskanamagus@gmail.com>
2016-07-30 23:17:03 -0700

    Avoid copying blank text

--- contrib/scripts/copy.lua
+++ contrib/scripts/copy.lua
@@ -3,7 +3,10 @@ function description()
 end
 
 function run()
-  windex = focused_window_index()
+  local windex = focused_window_index()
+  if windex == NOT_FOUND then
+    return false
+  end
   copy(webview_uri(windex, focused_webview_index(windex)))
   return true
 end
